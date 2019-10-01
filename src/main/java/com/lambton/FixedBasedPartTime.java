97f8594f200b70a34ae65e14ccc01812d2bb2c9a
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-01 11:00:11 -0700

    initialize variable and creation of constructor

--- src/main/java/com/lambton/FixedBasedPartTime.java
+++ src/main/java/com/lambton/FixedBasedPartTime.java
@@ -1,4 +1,16 @@
 package com.lambton;
 
 public class FixedBasedPartTime extends PartTime implements IPrintable {
+    int fixedAmount;
+
+
+    public FixedBasedPartTime(int employeeID, String employeeName, int employeeAge, float rate, int hoursWorked, int fixedAmount) {
+        super(employeeID, employeeName, employeeAge, rate, hoursWorked);
+        this.fixedAmount = fixedAmount;
+    }
+
+    @Override
+    public float calcEarnings() {
+        return 0;
+    }
 }
