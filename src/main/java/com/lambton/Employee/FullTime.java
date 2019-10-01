1715e963de01357272ff0ab5763115a10a5309a5
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-01 11:28:14 -0700

    getter and setter

--- src/main/java/com/lambton/Employee/FullTime.java
+++ src/main/java/com/lambton/Employee/FullTime.java
@@ -12,7 +12,22 @@ public class FullTime extends Employee implements IPrintable {
         this.salary = salary;
         this.bonus = bonus;
     }
-    
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public float getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(float bonus) {
+        this.bonus = bonus;
+    }
 
     @Override
     public float calcEarnings() {
