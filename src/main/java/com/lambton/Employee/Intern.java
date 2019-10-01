93bb0f8dca4c806d7314f47ff0ded799dd56688f
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-01 11:39:53 -0700

    new variable and constructor

--- src/main/java/com/lambton/Employee/Intern.java
+++ src/main/java/com/lambton/Employee/Intern.java
@@ -4,10 +4,13 @@ import com.lambton.IPrintable;
 
 public class Intern extends Employee implements IPrintable {
 
+    String school;
 
-    public Intern(int employeeID, String employeeName, int employeeAge) {
+    public Intern(int employeeID, String employeeName, int employeeAge, String school) {
         super(employeeID, employeeName, employeeAge);
+        this.school = school;
     }
+    
 
     @Override
     public float calcEarnings() {
