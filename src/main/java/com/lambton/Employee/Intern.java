233964261ca07313062fa0b9bd940eee40f6e823
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-01 11:38:33 -0700

    override and constructor

--- src/main/java/com/lambton/Employee/Intern.java
+++ src/main/java/com/lambton/Employee/Intern.java
@@ -3,4 +3,14 @@ package com.lambton.Employee;
 import com.lambton.IPrintable;
 
 public class Intern extends Employee implements IPrintable {
+
+
+    public Intern(int employeeID, String employeeName, int employeeAge) {
+        super(employeeID, employeeName, employeeAge);
+    }
+
+    @Override
+    public float calcEarnings() {
+        return 0;
+    }
 }
