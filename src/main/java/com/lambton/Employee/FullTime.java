3fccbbb3965384c0b528c38c050cfb450a11acf9
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-01 11:27:52 -0700

    initialize variables and make constructor

--- src/main/java/com/lambton/Employee/FullTime.java
+++ src/main/java/com/lambton/Employee/FullTime.java
@@ -3,10 +3,16 @@ package com.lambton.Employee;
 import com.lambton.IPrintable;
 
 public class FullTime extends Employee implements IPrintable {
-    
-    public FullTime(int employeeID, String employeeName, int employeeAge) {
+
+    float salary;
+    float bonus;
+
+    public FullTime(int employeeID, String employeeName, int employeeAge, float salary, float bonus) {
         super(employeeID, employeeName, employeeAge);
+        this.salary = salary;
+        this.bonus = bonus;
     }
+    
 
     @Override
     public float calcEarnings() {
