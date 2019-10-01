68bdeecdc2044a4b34c011afb04817be3094f021
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-09-30 17:32:09 -0700

    getter setter and function for calculating birthyear

--- src/main/java/com/lambton/Employee.java
+++ src/main/java/com/lambton/Employee.java
@@ -1,5 +1,7 @@
 package com.lambton;
 
+import java.time.LocalDate;
+
 public abstract class Employee {
 
     //declare variables
@@ -7,9 +9,53 @@ public abstract class Employee {
     private int employeeID;
     private char employeeName;
     private int employeeAge;
-    
+    private int BirthYear;
 
-    public abstract void  calcBirthYear();
+
+    public Employee(int employeeID, char employeeName, int employeeAge) {
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.employeeAge = employeeAge;
+    }
+
+
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public char getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(char employeeName) {
+        this.employeeName = employeeName;
+    }
+
+
+    public void setEmployeeAge(int employeeAge) {
+        this.employeeAge = employeeAge;
+    }
+
+    public int calcBirthYear()
+    {
+
+        LocalDate now = LocalDate.now();
+        int currentYear= now.getYear();
+        this.BirthYear=currentYear-this.employeeAge;
+        return this.BirthYear;
+
+
+
+    }
+
+    public int getBirthYear() {
+        return BirthYear;
+    }
 
     public abstract void calcEarnings();
 
