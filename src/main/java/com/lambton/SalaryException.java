300a813ea11a32bcd4f9a962ef3473b853037950
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-02 14:07:08 -0700

    initialize variable and setter

--- src/main/java/com/lambton/SalaryException.java
+++ src/main/java/com/lambton/SalaryException.java
@@ -1,4 +1,9 @@
 package com.lambton;
 
-public class SalaryException {
+public class SalaryException extends Exception {
+    String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
