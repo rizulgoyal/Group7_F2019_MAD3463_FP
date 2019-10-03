01820526a16916681549ed248e9357d92506e11f
Anmol <anmolsharma433@gmail.com>
2019-10-02 22:09:14 -0400

    vehicle constructor

--- src/main/java/com/lambton/Vehicle/Vehicle.java
+++ src/main/java/com/lambton/Vehicle/Vehicle.java
@@ -1,6 +1,21 @@
 package com.lambton.Vehicle;
 
 public abstract class Vehicle {
-    
+
+    //declaring variables
+    private String make;
+    private String plate;
+    private String insuranceCompany;
+    private float insuranceCost;
+
+    // Constructor
+
+    public Vehicle(String make, String plate, String insuranceCompany, float insuranceCost) {
+        this.make = make;
+        this.plate = plate;
+        this.insuranceCompany = insuranceCompany;
+        this.insuranceCost = insuranceCost;
+    }
+
 
 }
