efa23861c3f20cb37e3366755658fb25924e5c19
Anmol <anmolsharma433@gmail.com>
2019-10-02 22:33:03 -0400

    car class created and constructor added

---
+++ src/main/java/com/lambton/Vehicle/Car.java
@@ -0,0 +1,26 @@
+package com.lambton.Vehicle;
+
+public class Car extends Vehicle {
+    //declaring variables
+
+    private String carMaintainceCost;
+
+    //Constructor
+
+    public Car(String make, String plate, String insuranceCompany, float insuranceCost,String carMaintainceCost) {
+        super(make, plate, insuranceCompany, insuranceCost);
+        this.carMaintainceCost = carMaintainceCost;
+
+    }
+
+    //Getter and Setter
+
+    public String getCarMaintainceCost() {
+        return carMaintainceCost;
+    }
+
+    public void setCarMaintainceCost(String carMaintainceCost) {
+        this.carMaintainceCost = carMaintainceCost;
+    }
+
+}
