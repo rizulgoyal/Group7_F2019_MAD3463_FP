a9503c10195211e369ee0192907368f6a7c04f91
Anmol <anmolsharma433@gmail.com>
2019-10-02 22:15:12 -0400

    Iprintable interface Display method is implemented

--- src/main/java/com/lambton/Vehicle/Vehicle.java
+++ src/main/java/com/lambton/Vehicle/Vehicle.java
@@ -1,6 +1,8 @@
 package com.lambton.Vehicle;
 
-public abstract class Vehicle {
+import com.lambton.IPrintable;
+
+public abstract class Vehicle implements IPrintable {
 
     //declaring variables
     private String make;
@@ -17,5 +19,48 @@ public abstract class Vehicle {
         this.insuranceCost = insuranceCost;
     }
 
+    // Getter and Setter
 
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    public void setInsuranceCompany(String insuranceCompany) {
+        this.insuranceCompany = insuranceCompany;
+    }
+
+    public float getInsuranceCost() {
+        return insuranceCost;
+    }
+
+    public void setInsuranceCost(float insuranceCost) {
+        this.insuranceCost = insuranceCost;
+    }
+
+    //Disaplay function by using IPrintable Interface
+
+
+    @Override
+    public void printMyData() {
+        System.out.println("The Make is : " +getMake());
+        System.out.println("The Plate no is : "+getPlate());
+        System.out.println("The Insurance Company name is : "+getInsuranceCompany());
+        System.out.println("The Insurance Cost is : "+getInsuranceCost() );
+    }
 }
