364c659e3ec6673e7fd2d3f104a1be55d577650a
Anmol <anmolsharma433@gmail.com>
2019-10-04 18:35:14 -0400

    changes in employee package

---
+++ src/main/java/com/lambton/Employee/CommissionBasedPartTime.java
@@ -0,0 +1,14 @@
+package com.lambton.Employee;
+
+import com.lambton.Employee.PartTime;
+import com.lambton.IPrintable;
+
+public class CommissionBasedPartTime {
+    double commissionPercentage;
+    public void calculateEarnings(float rate,float commission,float hoursworked)
+    {
+
+    }
+
+    }
+
--- src/main/java/com/lambton/Employee/FixedBasedPartTime.java
+++ src/main/java/com/lambton/Employee/FixedBasedPartTime.java
@@ -3,39 +3,11 @@ package com.lambton.Employee;
 import com.lambton.Employee.PartTime;
 import com.lambton.IPrintable;
 
-public class FixedBasedPartTime extends PartTime implements IPrintable {
-
-    // create variables for stroing values
-    int fixedAmount;
-
-    // constructor to input all values
-    public FixedBasedPartTime(int employeeID, String employeeName, int employeeAge, float rate, int hoursWorked, int fixedAmount) {
-        super(employeeID, employeeName, employeeAge, rate, hoursWorked);
-        this.fixedAmount = fixedAmount;
-    }
-
-
-    //override abstract function to calculate salary
-    @Override
-    public float calcEarnings() {
-
-        float salary = this.getRate() * this.getHoursWorked();
-        float totalSalary = fixedAmount+salary;
-        return totalSalary;
-    }
-
-
-    //overriding interface function
-    @Override
-    public void printMyData() {
-
-        super.printMyData();
-        System.out.println("Employee is PartTime/Commision Based ");
-        System.out.println("Rate: " + this.getRate() + "$ per hour");
-        System.out.println("Hours Worked: " + this.getHoursWorked());
-        System.out.println("Fixed Amount: " + fixedAmount + "$");
-        System.out.println("Total Earnings: " + calcEarnings() + "$");
-
+public class FixedBasedPartTime {
+    double fixedAmount;
+    public void calcEarnings(float rate, float hoursWorked , float fixedAmount)
+    {
+        
     }
 
 }
