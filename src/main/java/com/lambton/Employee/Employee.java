61e7b742988a2aaf413c2b79eb654c716cfeb066
Anmol <anmolsharma433@gmail.com>
2019-10-04 18:28:40 -0400

    Employee class done

--- src/main/java/com/lambton/Employee/Employee.java
+++ src/main/java/com/lambton/Employee/Employee.java
@@ -1,81 +1,21 @@
 package com.lambton.Employee;
 
-import com.lambton.Vehicle.*;
-
-import com.lambton.IPrintable;
-
-import java.time.LocalDate;
-
-
-
-public abstract class Employee implements IPrintable {
+public abstract class Employee{
 
     //declare variables
 
-    private int employeeID;
-    private String employeeName;
-    private int employeeAge;
-    private int BirthYear;
+    String name;
+    int employeeAge;
+    float totalEarning;
 
-    //constructor
-    public Employee(int employeeID, String employeeName, int employeeAge) {
-        this.employeeID = employeeID;
-        this.employeeName = employeeName;
-        this.employeeAge = employeeAge;
-    }
-
-
-    // getter and setter
-    public int getEmployeeID() {
-        return employeeID;
-    }
-
-    public void setEmployeeID(int employeeID) {
-        this.employeeID = employeeID;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-
-    public void setEmployeeAge(int employeeAge) {
-        this.employeeAge = employeeAge;
-    }
-
-   // function to calculate year of birth
-    public int calcBirthYear()
+    public  String calculatebirthyear()
     {
-
-        LocalDate now = LocalDate.now();
-        int currentYear= now.getYear();
-        BirthYear=currentYear-employeeAge;
-        return BirthYear;
-
-
-
-    }
-    //overriding interface function
-    @Override
-    public void printMyData() {
-        System.out.println("ID: "+ getEmployeeID());
-        System.out.println("Name: "+getEmployeeName());
-        System.out.println("Birth Year: " + calcBirthYear());
-
+        return name;
     }
 
-    public abstract float calcEarnings();
-
-
-
-
-
-
-
+    public float calcEarnings() {
+        return totalEarning;
+    }
 
 
 }
