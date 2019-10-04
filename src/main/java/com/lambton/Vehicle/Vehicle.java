035620e4cffded99e8a45a3ab35b0bac2a882532
Anmol <anmolsharma433@gmail.com>
2019-10-04 18:01:35 -0400

    changed Vehicle class because of json

--- src/main/java/com/lambton/Vehicle/Vehicle.java
+++ src/main/java/com/lambton/Vehicle/Vehicle.java
@@ -5,45 +5,6 @@ import com.lambton.IPrintable;
 public abstract class Vehicle implements IPrintable {
 
     //declaring variables
-    private String make;
-    private String plate;
-
-
-    // Constructor
-
-    public Vehicle(String make, String plate) {
-        this.make = make;
-        this.plate = plate;
+     String make;
+     String plate;
     }
-
-    // Getter and Setter
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getPlate() {
-        return plate;
-    }
-
-    public void setPlate(String plate) {
-        this.plate = plate;
-    }
-
-
-    //Display function by using IPrintable Interface
-
-
-    @Override
-    public void printMyData() {
-        System.out.println("The Make is : " +getMake());
-        System.out.println("The Plate no is : "+getPlate());
-
-    }
-
-
-}
