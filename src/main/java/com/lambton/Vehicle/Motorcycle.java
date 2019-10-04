ac061cfc9a3462cbf2052e52d00b651a68a9fbc6
Anmol <anmolsharma433@gmail.com>
2019-10-04 18:21:21 -0400

    Motorcycle class changed

--- src/main/java/com/lambton/Vehicle/Motorcycle.java
+++ src/main/java/com/lambton/Vehicle/Motorcycle.java
@@ -1,65 +1,40 @@
 package com.lambton.Vehicle;
 
-public class Motorcycle extends Vehicle {
+import com.lambton.IPrintable;
+import org.json.simple.JSONObject;
+
+public class Motorcycle extends Vehicle implements IPrintable{
     //declaring variables
 
-    private int carMaintainceCost;
-    private String insuranceCompany;
-    private Boolean insurance;
+    JSONObject jsonObject;
+    String model;
+    String make;
+    Boolean insurance;
 
     //constructor
 
-    public Motorcycle(String make, String plate, int carMaintainceCost, String insuranceCompany, Boolean insurance) {
-        super(make, plate);
-        this.carMaintainceCost = carMaintainceCost;
-        this.insuranceCompany = insuranceCompany;
-        this.insurance = insurance;
-    }
 
-    //Extra Constructor because boolean is not proper functionable
-    public Motorcycle(String make, String plate, int carMaintainceCost, String insuranceCompany) {
-        super(make, plate);
-        this.carMaintainceCost = carMaintainceCost;
-        this.insuranceCompany = insuranceCompany;
-
-    }
-
-    //Getter and Setter
-
-
-    public int getCarMaintainceCost() {
-        return carMaintainceCost;
-    }
-
-    public void setCarMaintainceCost(int carMaintainceCost) {
-        this.carMaintainceCost = carMaintainceCost;
-    }
-
-    public String getInsuranceCompany() {
-        return insuranceCompany;
-    }
-
-    public void setInsuranceCompany(String insuranceCompany) {
-        this.insuranceCompany = insuranceCompany;
-    }
-
-    public Boolean getInsurance() {
-        return insurance;
-    }
-
-    public void setInsurance(Boolean insurance) {
-        this.insurance = insurance;
+    public Motorcycle(JSONObject jsonObject) {
+        this.jsonObject = jsonObject;
+        super.make= (String) jsonObject.get("make");
+        super.plate = (String) jsonObject.get("plate");
+        model = (String) jsonObject.get("model");
+        insurance = (Boolean) jsonObject.get("insurance");
     }
 
 
-    //Display function by using IPrintable Interface
+    //iprintable method
+
 
     @Override
     public void printMyData() {
-        System.out.println("The Make is : " +this.getMake());
-        System.out.println("The Plate no is : "+this.getPlate());
-        System.out.println("The Insurance Company name is : " +getInsuranceCompany());
-        System.out.println("The Insurance is : "+getInsurance());
-        System.out.println("The car maintaince cost is : "+getCarMaintainceCost());
+            System.out.println("Has a " +jsonObject.get("type") );
+            System.out.println("Manufacturer Name : " + make );
+            System.out.println("Licence Plate : " +plate);
+            System.out.println("Model Code :" +model);
+            if(insurance=true)
+                System.out.println("Insurance : Yes");
+            else
+                System.out.println("Insurance : No");
     }
 }
