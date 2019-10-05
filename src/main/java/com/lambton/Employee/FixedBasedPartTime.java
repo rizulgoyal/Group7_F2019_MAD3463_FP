a3b382ed1e052138547201a9683f1e0d03561db6
rizulgoyal <55304035+rizulgoyal@users.noreply.github.com>
2019-10-04 18:24:56 -0700

    try and catch on hourly pay on parttime

--- src/main/java/com/lambton/Employee/FixedBasedPartTime.java
+++ src/main/java/com/lambton/Employee/FixedBasedPartTime.java
@@ -2,6 +2,7 @@ package com.lambton.Employee;
 
 import com.lambton.Employee.PartTime;
 import com.lambton.IPrintable;
+import com.lambton.SalaryException;
 
 public final class FixedBasedPartTime extends PartTime implements IPrintable {
 
@@ -34,7 +35,23 @@ public final class FixedBasedPartTime extends PartTime implements IPrintable {
         System.out.println("Employee is PartTime/Commision Based ");
         System.out.println("Rate: " + this.getRate() + "$ per hour");
         System.out.println("Hours Worked: " + this.getHoursWorked());
-        System.out.println("Fixed Amount: " + fixedAmount + "$");
+        try{
+            if(fixedAmount<10)
+            {
+                throw  new SalaryException();
+            }
+            else
+            {
+                System.out.println("Fixed Amount: " + fixedAmount + "$");            }
+
+
+        }
+        catch (SalaryException e)
+        {
+            System.out.println(fixedAmount+"$ Salary is not acceptable");
+        }
+
+
         System.out.println("Total Earnings: " + calcEarnings() + "$");
 
     }
